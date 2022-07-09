82b341f37e7a68c4a28b38066669a3a896c65848
YourfriendRND <kupeirc@gmail.com>
2022-07-09 20:41:36 +0300

    Добавил компоненты страниц в приложение

---
+++ project/src/contants.ts
@@ -0,0 +1,7 @@
+// Модуль для констант приложения
+
+const CARD_QTY_ON_PAGE = 20;
+
+export {
+  CARD_QTY_ON_PAGE
+};
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const PromoFilmData = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  YEAR: 2014
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App title={PromoFilmData.TITLE} genre={PromoFilmData.GENRE} year={PromoFilmData.YEAR} />
   </React.StrictMode>,
 );
