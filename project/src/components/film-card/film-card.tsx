fb67f632ec1404eb4983e51bb5d8ccb941f874d7
YourfriendRND <kupeirc@gmail.com>
2022-07-21 18:21:32 +0300

    добавил компонент формы WIP

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,19 +1,23 @@
 import { Link } from 'react-router-dom';
-import {Film} from '../../types/film';
+import { Film } from '../../types/film';
+import { useState } from 'react';
 
-type FilmProp={
+type FilmProp = {
   film: Film
 }
 
-const FilmCard = ({film}:FilmProp):JSX.Element => (
-  <article className="small-film-card catalog__films-card">
-    <div className="small-film-card__image">
-      <img src={film.previewImage} alt={film.name} width="280" height="175" />
-    </div>
-    <h3 className="small-film-card__title">
-      <Link className="small-film-card__link" to={`films/${film.id}`}>{film.name}</Link>
-    </h3>
-  </article>
-);
+const FilmCard = ({ film }: FilmProp): JSX.Element => {
+  const [, setActiveFilm] = useState({id: 1});
+  return (
+    <article className="small-film-card catalog__films-card" id={`${film.id}`} onMouseOver={() => setActiveFilm({id: film.id})}>
+      <div className="small-film-card__image">
+        <img src={film.previewImage} alt={film.name} width="280" height="175" />
+      </div>
+      <h3 className="small-film-card__title">
+        <Link className="small-film-card__link" to={`films/${film.id}`}>{film.name}</Link>
+      </h3>
+    </article>
+  );
+};
 
 export default FilmCard;
--- project/src/components/film-list/film-list.tsx
+++ project/src/components/film-list/film-list.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react';
 import FilmCard from '../film-card/film-card';
 import { Film } from '../../types/film';
-
 
 type FilmListProp = {
   films: Film[]
 };
 
-const FilmList = ({ films }: FilmListProp): JSX.Element => {
-  const [filmActive, setFilmActive] = useState({id: 1});
-  return (
-    <div className="catalog__films-list">
-      {films.map((film) => <FilmCard key={film.id} film={film} />)}
-    </div>
-  );
-};
+const FilmList = ({ films }: FilmListProp): JSX.Element => (
+  <div className="catalog__films-list">
+    {films.map((film) => <FilmCard key={film.id} film={film} />)}
+  </div>
+);
 
 export default FilmList;
